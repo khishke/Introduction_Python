f7c8ef5ef582b3eec144ad21c589d7414def5bdf
Py4Econmn <Py4Econmn>
2021-12-11 10:45:24 +0100

    small edits during lesson

--- 1_Introduction/code.py
+++ 1_Introduction/code.py
@@ -1,9 +1,25 @@
 # Sample code
 
 print("Line 1")
-a = 17 			# number of students
-print(a)
+# number of students
+# print(a)
 print("Line low")
+print('Lower a is: ', a)
+print('Lower in format a is: {}'.format(a))
 
-m = input("Enter a number: ")
-print("You entered: " + str(m) + '. Done!')
+1+2
+5-9
+5*5
+5/8
+5**2
+5**(1/2)
+
+my_condition0 = True
+my_condition1 = False
+
+print(my_condition1)
+
+# c = 'hello'
+
+# m = input("Enter a number: ")
+# print("You entered: " + str(m) + '. Done!')
