fa44e33d939d276499cbaba599a8d602648a0196
Py4Econmn <Py4Econmn>
2021-12-19 20:06:55 +0100

    before lecture 5, 19 Dec

--- 5_Visualization/basic_seaborn.py
+++ 5_Visualization/basic_seaborn.py
@@ -8,3 +8,17 @@ tips = sns.load_dataset("tips")
 
 sns.regplot(x="total_bill", y="tip", data=tips);
 plt.show()
+
+# libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+from gapminder import gapminder # data set
+
+# data
+data = gapminder.loc[gapminder.year == 2007]
+
+# use the scatterplot function to build the bubble map
+sns.scatterplot(data=data, x="gdpPercap", y="lifeExp", size="pop", legend=False, sizes=(20, 2000))
+
+# show the graph
+plt.show()
