30efcba7cd17fe1be3568c64da7f12726d6baeac
py4econ@gmail.com <Py4Econmn>
2022-10-16 12:53:38 +0200

    before lesson 2

--- _Function_Class_and_Tools/practice/exception.py
+++
@@ -1,5 +0,0 @@
-# random number
-# works only if the sum is positive
-# throw error otherwise
-# take absolute value if above 0.1
-# leave if below 0.1
--- _Function_Class_and_Tools/practice/one_liner.py
+++
@@ -1,2 +0,0 @@
-# append same string to each element of list
-# select only strings (positive numbers) from list in one line
