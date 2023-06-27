e30456441ccbd29ded363e82ec0a7b289ad47c9a
Khishke <khishigjargalbatbaatar@gmail.com>
2023-06-27 07:44:40 -0500

    added new file

--- 1_Introduction/code/run_code.py
+++ 1_Introduction/code/run_code.py
@@ -9,3 +9,16 @@ num = float(input('Enter a number that you want a square root of: '))
 num_sqrt = num ** 0.5 
 
 print('The the square root of ' + str(num) + ' is ' + str(num_sqrt) + '.')
+
+print("say hi")
+
+2+3
+
+#khishke
+
+##bi ym nemeeed bn####
+
+5-0
+
+12-8
+
